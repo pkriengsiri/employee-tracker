133232118f018946faec7a604d2a2071ae6fcea8
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-20 20:10:46 -0500

    Added showAllEmployees function to index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
 
-
 // MAKE CONNECTION TO THE DATABASE
 const connection = mysql.createConnection({
   host: "localhost",
@@ -25,12 +24,29 @@ connection.connect(function (err) {
   init();
 });
 
-
-function quit() {
-    console.log("Goodbye.\n Have a nice day.");
-    connection.end();
+// Shows all employees
+function showAllEmployees() {
+  const queryString = `SELECT e.first_name as 'First Name', e.last_name 'Last Name', r.title as 'Role', r.salary as 'Salary', m.first_name as 'Manager First Name', m.last_name as 'Manager Last Name'
+    FROM employee e
+    INNER JOIN employee m
+        on e.id = m.id
+    INNER JOIN role r
+        on r.id = e.role_id;
+    `;
+  connection.query(queryString, (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    init();
+  });
 }
 
+// Exits the application
+function quit() {
+  console.log("Goodbye.\nHave a nice day.");
+  connection.end();
+}
+
+// Starts the application
 function init() {
   inquirer
     .prompt([
@@ -43,11 +59,11 @@ function init() {
     ])
     .then(({ userInput }) => {
       switch (userInput) {
-          case "Show all employees":
-              showAllEmployees()
-              return;
-          default:
-                  
+        case "Show all employees":
+          showAllEmployees();
+          return;
+        default:
+          quit();
       }
     });
 }
